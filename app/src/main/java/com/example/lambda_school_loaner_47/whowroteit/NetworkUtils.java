eca48aa3d6a9846adc5d632f52eb3f27f890c057
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-10 01:48:15 -0600

    used uri builder to build url

--- app/src/main/java/com/example/lambda_school_loaner_47/whowroteit/NetworkUtils.java
+++ app/src/main/java/com/example/lambda_school_loaner_47/whowroteit/NetworkUtils.java
@@ -1,13 +1,20 @@
 package com.example.lambda_school_loaner_47.whowroteit;
 
+import android.net.Uri;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class NetworkUtils {
     
     private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
-    
+    private static final String BOOK_BASE_URL = "https://googleapis.com/books/v1/volumes?";
+    private static final String QUERY_PARAM = "q";
+    private static final String MAX_RESULTS = "maxResults";
+    private static final String PRINT_TYPE = "printType";
+
     static String getBookInfo(String queryString){
         
         HttpURLConnection urlConnection  = null;
@@ -15,11 +22,20 @@ public class NetworkUtils {
         String            bookJsonString = null;
         
         try {
-            // TODO: 1/10/19  
+            // TODO: 1/10/19 use a uri builder to build uri
+            Uri builtURI = Uri.parse(BOOK_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, queryString)
+                    .appendQueryParameter(MAX_RESULTS, "10")
+                    .appendQueryParameter(PRINT_TYPE, "books")
+                    .build();
+
+            //convert uri to a url
+            URL requesturl = new URL(builtURI.toString());
+
         }catch (IOException e) {
             e.printStackTrace();
         }finally {
-            // TODO: 1/10/19  
+            // TODO: 1/10/19
         }
         return bookJsonString;
     }
