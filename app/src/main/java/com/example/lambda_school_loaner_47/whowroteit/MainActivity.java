324e516fa1a6cdc2d6a7f53bf5017077e693daf7
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-10 00:46:24 -0600

    get query from edit_text_view

--- app/src/main/java/com/example/lambda_school_loaner_47/whowroteit/MainActivity.java
+++ app/src/main/java/com/example/lambda_school_loaner_47/whowroteit/MainActivity.java
@@ -3,15 +3,27 @@ package com.example.lambda_school_loaner_47.whowroteit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mBookInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBookInput  = findViewById(R.id.bookInput);
+        mTitleText  = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.authorText);
     }
 
     public void searchBooks(View view) {
+        //Get the search string from the input field
+        String queryString = mBookInput.getText().toString();
     }
 }
