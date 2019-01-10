ef08de6163035c98778474f4f16f8fe9e6161720
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-10 09:56:43 -0600

    added get load manager to main on_create

--- app/src/main/java/com/example/lambda_school_loaner_47/whowroteitloader/MainActivity.java
+++ app/src/main/java/com/example/lambda_school_loaner_47/whowroteitloader/MainActivity.java
@@ -35,6 +35,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mBookInput  = findViewById(R.id.bookInput);
         mTitleText  = findViewById(R.id.titleText);
         mAuthorText = findViewById(R.id.authorText);
+
+        if (getSupportLoaderManager().getLoader(0) != null){
+            getSupportLoaderManager().initLoader(0, null, this);
+        }
     }
 
     public void searchBooks(View view) {
